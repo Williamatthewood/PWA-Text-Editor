bc161c853c4bc7ceee07573975acd2109a51a8b6
Williamatthewood <w.wood@tcu.edu>
2023-07-03 15:28:57 -0500

    added comments

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -12,6 +12,7 @@ module.exports = () => {
     entry: {
       main: './src/js/index.js',
       install: './src/js/install.js'
+      //editor?
     },
     output: {
       filename: '[name].bundle.js',

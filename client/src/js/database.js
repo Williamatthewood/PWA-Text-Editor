5c9189d29aeabb14ffc4ff950d02e99f5dc00631
Williamatthewood <w.wood@tcu.edu>
2023-07-06 18:02:40 -0500

    fixes for saving issue. Application seems to be working. Need to test on heroku

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -7,7 +7,7 @@ const initdb = async () =>
         console.log('jate database already exists');
         return;
       }
-      db.createObjectStore('jate', { keyPath: 'id' });
+      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
       console.log('jate database created');
     },
   });
@@ -21,15 +21,13 @@ const tx = editorDb.transaction('jate', 'readwrite');
 
 const store = tx.objectStore('jate');
 
-const request = store.put({ id: 'content', content: content });
+const request = store.put({ id: 1, value: content });
 
 const result = await request;
 
-console.log('data saved to the database', result);
-
+console.log('data saved to the database', result.value);
 
 }
-
 
 export const getDb = async () => {
   console.log('GET from the database');
@@ -40,11 +38,11 @@ export const getDb = async () => {
 
   const store = tx.objectStore('jate');
 
-  const request = store.getAll();
+  const request = store.get(1);
 
   const result = await request;
   console.log('result.value', result);
-  return result;
+  return result.value;
 }
 
 initdb();
